66739899438741bd6d9a0aefc9ba5c0a0743b150
jbernsen <jeroen.bernsen@gmail.com>
2024-03-22 12:11:07 +0100

    Propsed fix works!

---
+++ iOSLibWarnings/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Silence catalyst warnings: https://github.com/xamarin/xamarin-macios/issues/20314
+[assembly: System.Runtime.Versioning.UnsupportedOSPlatform("maccatalyst")]
